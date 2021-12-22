9afbd122326f2df712c59129f07cfd6b04cf999f
Feliphe Silva <feliphevs90@outlook.com>
2021-12-22 14:11:20 -0300

    correcao index

--- src/action.js
+++ src/action.js
@@ -1,27 +1,33 @@
-const fetch = require('node-fetch');
 const core = require('@actions/core');
-const github = require ('@actions/github');
+const github = require('@actions/github');
+const fetch = require('node-fetch');
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
+    var results;
 
-    const randomPos = Math.round(Math.random() * 1000);
-    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-    const response = await fetch(url);
-    const { results } = await response.json();
-    const gifUrl = results[0].media[0].tinygif.url;
+    do{
+      const randomPos = Math.round(Math.random() * 10);
+      const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+      const response = await fetch(url);
+      results = await response.json();
+    }while(results['next'] === "0" );
+ 
+    const gifUrl = results['results'][0]['media'][0]['tinygif']['url'];
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
-    const { pull_request } = context.payload;
-
+    const { pull_request } = context.payload;  
+    
     await octokit.rest.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: `Obrigado por enviar esta pull request. Vamos revisá-la o mais breve possível.\n\n<img src="${gifUrl}" alt="thank you" />`
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: `Obrigado pela pull request, vamos analiza-la o mais rapido possivel.\n\n<img src="${gifUrl}" alt="thank you" />`
     });
-}
+  }
 
-run();
+
+  
+  run();
