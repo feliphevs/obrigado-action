4b0b1232f6a9ff65719507b2b2d3b998b1b37aa2
Feliphe Silva <feliphevs90@outlook.com>
2021-12-22 11:41:50 -0300

    adicionar gifs a note de agradecimento

--- src/action.js
+++ src/action.js
@@ -1,8 +1,16 @@
+const fetch = require('node-fetch');
 const core = require('@actions/core');
 const github = require ('@actions/github');
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
+
+    const randomPos = Math.round(Math.random() * 1000);
+    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+    const response = await fetch(url);
+    const { results } = await response.json();
+    const gifUrl = results[0].media[0].tinygif.url;
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
@@ -12,7 +20,7 @@ async function run() {
     await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
-        body: 'Obrigado por enviar esta pull request. Vamos revisá-la o mais breve possível'
+        body: `Obrigado por enviar esta pull request. Vamos revisá-la o mais breve possível.\n\n<img src="${gifUrl}" alt="thank you" />`
     });
 }
 
