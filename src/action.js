2fdab2b3fb4f652ea161c3e8ffa39d87c24d69f6
Feliphe Silva <feliphevs90@outlook.com>
2021-12-22 08:33:12 -0300

    corrigir run do action.js

--- src/action.js
+++ src/action.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require ('@actions/github');
 
-async function run (){
+async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
